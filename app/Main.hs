012330aa3caaceb4d7947edd0568261f11702872
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-08-16 21:51:09 -0700

    Add Hoogle support

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,11 @@ main =
   scotty 3000 $ do
     get "/" $
       html $ renderHtml homepage
-    get "/:word" $ do
-      word <- param "word"
-      html $ mconcat ["<h1>Hello, ", word, "!</h1>"]
+    get "/events/:event" $ do
+      event <- param "event"
+      html $ mconcat ["<h1>Hello, ", event, "!</h1>"]
+    get "/events" $ do
+      html $ mconcat ["<h1>Events ",  "!</h1>"]
+    post "/events" $ do
+      event <- param "event"
+      html $ mconcat ["<h1>Event, ", event, "!</h1>"]
--- shell.nix
+++ shell.nix
@@ -1,14 +1,27 @@
 { sources ? import ./nix/sources.nix {},
-  pkgs ? import sources.nixpkgs {}}:
+  pkgs ? import sources.nixpkgs {},
+  withHoogle ? true}:
 let
   inherit  pkgs;
   inherit (pkgs) haskellPackages;
-
-  project = import ./release.nix;
+  f = import ./default.nix;
+  packageSet = pkgs.haskellPackages;
+  hspkgs = (
+    if withHoogle then
+      packageSet.override {
+        overrides = (self: super: {
+          ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+          ghcWithPackages = self.ghc.withPackages;
+        });
+      }
+      else packageSet
+  );
+  drv = hspkgs.callPackage f {};
 in
 pkgs.stdenv.mkDerivation {
   name = "shell";
-  buildInputs = project.env.nativeBuildInputs ++ [
+
+  buildInputs = drv.env.nativeBuildInputs ++ [
     haskellPackages.cabal-install
     haskellPackages.haskell-language-server
     haskellPackages.ghcid
